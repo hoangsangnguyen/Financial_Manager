f8711ad9c9b1effc6391e821e7aa9951fc934b31
Nhat Truong <nhattruong251295@gmail.com>
2018-03-18 22:46:02 +0700

    Update home screen

--- FinancialManager/app/src/main/java/com/example/nhattruong/financialmanager/mvp/home/HomeActivity.java
+++ FinancialManager/app/src/main/java/com/example/nhattruong/financialmanager/mvp/home/HomeActivity.java
@@ -1,11 +1,39 @@
 package com.example.nhattruong.financialmanager.mvp.home;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.example.nhattruong.financialmanager.R;
 import com.example.nhattruong.financialmanager.base.BaseActivity;
+import com.example.nhattruong.financialmanager.mvp.home.adapter.JarAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.BindView;
 
 public class HomeActivity extends BaseActivity implements HomeContract.View {
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
+    @BindView(R.id.navigation_view)
+    NavigationView navigationView;
+
+    @BindView(R.id.tb_home)
+    Toolbar toolbar;
+
+    @BindView(R.id.rcv_jar)
+    RecyclerView rcvJar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +49,52 @@ public class HomeActivity extends BaseActivity implements HomeContract.View {
 
     @Override
     public void onInitData() {
+        toolbar.setTitle(getString(R.string.app_name));
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
+
+        List<String> list = new ArrayList<>(Arrays.asList("aa", "bb", "cc", "dd", "ee", "ff"));
+
+        rcvJar.setLayoutManager(new GridLayoutManager(this, 2));
+        rcvJar.setAdapter(new JarAdapter(this, list, new JarAdapter.Listener() {
+            @Override
+            public void onItemClicked(int position) {
+                navigationView.getMenu().getItem(position).setChecked(true);
+            }
+        }));
     }
 
     @Override
     public void onInitListener() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                drawerLayout.closeDrawers();
+//                switch (item.getItemId()){
+//                    case R.id.jar1:
+//                        break;
+//                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
 
     }
 }
---
+++ FinancialManager/app/src/main/java/com/example/nhattruong/financialmanager/mvp/home/adapter/JarAdapter.java
@@ -0,0 +1,71 @@
+package com.example.nhattruong.financialmanager.mvp.home.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.nhattruong.financialmanager.R;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class JarAdapter extends RecyclerView.Adapter<JarAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<String> mItems;
+    private Listener mCallback;
+
+    public JarAdapter(Context mContext, List<String> mItems, Listener listener) {
+        this.mContext = mContext;
+        this.mItems = mItems;
+        this.mCallback = listener;
+    }
+
+    @Override
+    public JarAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.item_jar, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(JarAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems == null ? 0 : mItems.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.iv_jar)
+        ImageView ivJar;
+
+        @BindView(R.id.tv_quantity_money)
+        TextView tvQuantity;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (mCallback != null) {
+                        mCallback.onItemClicked(getAdapterPosition());
+                    }
+                }
+            });
+        }
+    }
+
+    public interface Listener {
+        void onItemClicked(int position);
+    }
+}
